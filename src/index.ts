afe408c77517e25f17b5fa144e21c63cba1e13e5
denizat <deniza.telci@gmail.com>
2021-05-10 16:30:53 -0500

    Import order comments.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+// First because most obvious element
 import "./links.js";
+// Second because most important interaction
 import "./keyHandler.js";
+// Last because least obvious and necessary element
 import "./modes.js";
