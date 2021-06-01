f28cf09cb397a325ee8fb18e74699b3b17ded08d
denizat <deniza.telci@gmail.com>
2021-05-31 22:33:28 -0500

    Initial changes

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,75 @@
-import { cfg } from "./config";
+import { Element, render } from "@denizat/node_html";
+import { links } from "./links.js";
 
-if (cfg.grabMouse) {
-  chrome.storage.sync.get(["newPage"], (result) => {
-    if (result.newPage) {
-      chrome.tabs.create({ url: "index.html" }, null);
-      window.close();
-      chrome.storage.sync.set({ newPage: false }, null);
-    } else {
-      chrome.storage.sync.set({ newPage: true }, null);
-    }
-  });
-}
+const html = new Element("html");
+html.appendChild(links);
 
-if (cfg.startWithFocusOnSearchBar) {
-  document.getElementById("input_box").focus();
-}
+const inner = `
 
-if (!cfg.zenMode) {
-  import("./links").then();
-} else {
-  document.getElementById("root").style.opacity = "0";
-}
-import "./modes.ts";
+      <div class="search-box">
+        <select name="mode" id="mode" autocomplete></select>
+        <span>[m]</span>
+        <form id="search" class="search" method="get">
+          <input
+            id="input_box"
+            class="input_box"
+            type="text"
+            name="q"
+            autocomplete="off"
+          />
+        </form>
+        <span>[s]</span>
+      </div>
+      <div id="container" class="container"></div>
 
-import "./keyHandler.ts";
-import "./searchHandler.ts";
+`;
+
+console.log(
+  `
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="./styles/Typewriter.css" />
+    <title>ホームページ</title>
+  </head>
+  <body>
+    <div id="root">
+    ${render(html)}
+    ${inner}
+
+          </div>
+  </body>
+</html>
+`
+);
+
+// import { cfg } from "./config";
+
+// if (cfg.grabMouse) {
+//   chrome.storage.sync.get(["newPage"], (result) => {
+//     if (result.newPage) {
+//       chrome.tabs.create({ url: "index.html" }, null);
+//       window.close();
+//       chrome.storage.sync.set({ newPage: false }, null);
+//     } else {
+//       chrome.storage.sync.set({ newPage: true }, null);
+//     }
+//   });
+// }
+
+// if (cfg.startWithFocusOnSearchBar) {
+//   document.getElementById("input_box").focus();
+// }
+
+// if (!cfg.zenMode) {
+//   import("./links").then();
+// } else {
+//   document.getElementById("root").style.opacity = "0";
+// }
+// import "./modes.ts";
+
+// import "./keyHandler.ts";
+// import "./searchHandler.ts";
--- src/links.ts
+++ src/links.ts
@@ -1,4 +1,5 @@
-import { cfg } from "./config";
+import { cfg } from "./config.js";
+import { Element, render } from "@denizat/node_html";
 
 // Gets the max row among all columns so that we can have them be an even height, might remove this if there is a css solution but I don't know css so its here for now.
 let max = 0;
@@ -8,32 +9,36 @@ cfg.bookMarks.forEach((column) => {
   }
 });
 
-let container = document.getElementById("container");
+// let container = document.getElementById("container");
+let container = new Element("container");
+container.setAttribute("class", "container");
 cfg.bookMarks.forEach((column) => {
-  let col = document.createElement("div");
-  let text = document.createElement("p");
+  let col = new Element("div");
+  let text = new Element("p");
   if (column.hidden !== true) {
-    text.innerHTML = `${column.name} [${column.hotkey[0]}]`;
+    text.appendChild(`${column.name} [${column.hotkey[0]}]`);
   }
 
   col.appendChild(text);
 
   column.rows.forEach((row) => {
-    let link = document.createElement("a");
-    link.className = "link";
+    let link = new Element("a");
+    link.setAttribute("class", "link");
     if (row.hidden !== true && column.hidden !== true) {
       link.setAttribute("href", row.link);
-      link.innerHTML = `${row.name} [${row.hotkey[0]}]`;
+      link.appendChild(`${row.name} [${row.hotkey[0]}]`);
     }
     col.appendChild(link);
     if (0 < column.rows.length % max) {
       for (let i = 0; i < (column.rows.length % max) + 1; i++) {
-        let link = document.createElement("a");
-        link.className = "link null";
-        link.innerHTML = "link null";
+        let link = new Element("a");
+        link.setAttribute("class", "link null");
+        link.appendChild("link null");
         col.appendChild(link);
       }
     }
   });
   container.appendChild(col);
 });
+
+export const links = container;
